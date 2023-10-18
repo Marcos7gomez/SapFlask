b47f351942d7d2fec3a0a5d9c3894cc9978da173
Marcos Gomez <marcoslg.gomez@gmail.com>
2023-10-18 18:10:13 -0300

    Login y register terminados y funcionando

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request, url_for
-from flask_migrate import Migrate
+import re
+
 import psycopg2
 import psycopg2.extras
+from flask import Flask, render_template, request, url_for, flash, session
+from flask_migrate import Migrate
+# from werkzeug.security import check_password_hash, generate_password_hash
+#from sqlalchemy.testing.pickleable import User
+from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
 from database import db
@@ -38,6 +43,8 @@ app.config['SECRET_KEY'] = 'llave_secreta'
 @app.route('/index.html')
 @app.route('/')
 def home():
+    if 'loggedin' in session:
+        return render_template('home2.html', usuario=session['usuario'])
     return redirect(url_for('login'))
 
 
@@ -78,7 +85,7 @@ def agregar():
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    # Verifica si usuario y contrasenia existen
+    #Verifica si usuario y contrasenia existen
     if request.method == 'POST' and 'usuario' in request.form and 'contrasenia' in request.form:
         usuario = request.form['usuario']
         contrasenia = request.form['contrasenia']
@@ -88,8 +95,58 @@ def login():
         # Obtiene un registro y da un resultado
         cuenta = cursor.fetchone()
         if cuenta:
-            return redirect(url_for('inicio'))
+            password_rs = cuenta['contrasenia']
+            print(password_rs)
+            #Si la cuenta existe en usuario de la bd
+            if check_password_hash(password_rs, contrasenia):
+                session['loggedin'] = True
+                session['id'] = cuenta['id']
+                session['usuario'] = cuenta['usuario']
+                print('Sesión exitosa')
+                return redirect(url_for('home'))
+            else:
+                flash('¡La cuenta ingresada no existe!')
         else:
-            print('¡¡Error!! ¡¡La cuenta ingresada no existe!!')
-            return redirect(url_for('login'))
+            flash('¡La cuenta ingresada no existe!')
+        # return redirect(url_for('login'))
     return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'usuario' in request.form and 'contrasenia' in request.form and 'email' \
+            in request.form:
+        # Create variables for easy access
+        usuario = request.form['usuario']
+        departamento = request.form['departamento']
+        contrasenia = request.form['contrasenia']
+        email = request.form['email']
+
+        _hashed_password = generate_password_hash(contrasenia)
+
+        # Check if account exists using MySQL
+        cursor.execute('SELECT * FROM persona WHERE usuario = %s', (usuario,))
+        cuenta = cursor.fetchone()
+        print(cuenta)
+        # If account exists show error and validation checks
+        if cuenta:
+            flash('¡Esta cuenta ya existe!!')
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            flash('Dirección de email inválida!')
+        elif not re.match(r'[A-Za-z0-9]+', usuario):
+            flash('EL usuario debe contener sólo palabras y números!')
+        elif not contrasenia or not contrasenia or not email:
+            flash('¡Por favor rellena el formulario!')
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into users table
+            cursor.execute("INSERT INTO persona (usuario, departamento, contrasenia, email) VALUES (%s,%s,%s,%s)",
+                           (usuario, departamento, _hashed_password, email))
+            conn.commit()
+            flash('You have successfully registered!')
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        flash('Please fill out the form!')
+    # Show registration form with message (if any)
+    return render_template('register.html')
